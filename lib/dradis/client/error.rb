61d971e3d2e4fbd60e53b3470a7dabbdcd0f63d5
Daniel Martin <etd-github@nomejortu.com>
2014-04-15 15:01:36 +0200

    Error - new error type: Unauthorized

--- lib/dradis/client/error.rb
+++ lib/dradis/client/error.rb
@@ -4,6 +4,42 @@ module Dradis
     class Error < StandardError
       attr_reader :code
 
+
+      # --------------------------------------------------------- Class methods
+
+      # Create a new error from an HTTP response
+      #
+      # @param response [Faraday::Response]
+      # @return [Dradis::Client::Error]
+      def self.from_response(response)
+        message, code = parse_error(response.body)
+        # new(message, response.response_headers, code)
+        new(message, code)
+      end
+
+      # @return [Hash]
+      def self.errors
+        @errors ||= {
+                  # 400 => Twitter::Error::BadRequest,
+                  401 => Dradis::Client::Error::Unauthorized,
+                  # 403 => Twitter::Error::Forbidden,
+                  # 404 => Twitter::Error::NotFound,
+                  # 406 => Twitter::Error::NotAcceptable,
+                  # 408 => Twitter::Error::RequestTimeout,
+                  # 422 => Twitter::Error::UnprocessableEntity,
+                  # 429 => Twitter::Error::TooManyRequests,
+                  # 500 => Twitter::Error::InternalServerError,
+                  # 502 => Twitter::Error::BadGateway,
+                  # 503 => Twitter::Error::ServiceUnavailable,
+                  # 504 => Twitter::Error::GatewayTimeout,
+                }
+      end
+
+
+
+
+      # ------------------------------------------------------ Instance methods
+
       # Initializes a new Error object
       #
       # @param exception [Exception, String]
@@ -20,6 +56,21 @@ module Dradis
       # Raised when Dradis returns a 4xx HTTP status code
       class ClientError < self; end
 
+      # Raised when Dradis returns the HTTP status code 401
+      class Unauthorized < ClientError; end
+
+
+      private
+
+      def self.parse_error(body)
+        if body.nil?
+          ['', nil]
+        elsif body[:message]
+          [body[:message], nil]
+        # elsif body[:errors]
+        #   extract_message_from_errors(body)
+        end
+      end
     end
   end
 end
